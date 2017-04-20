1877a70906114679e5721447c4e8e22632eb5d21
ruhenheim <asd.uchiyama@gmail.com>
2017-04-20 13:09:29 +0900

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
+
+  private
+  def require_user_logged_in
+    redirect_to login_url unless logged_in?
+  end
 end
