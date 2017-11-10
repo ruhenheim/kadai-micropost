cce454c960003525dcc6652603a53a25da5cdc8f
ruhenheim <asd.uchiyama@gmail.com>
2017-11-10 18:17:30 +0900

    seed for user x 10, micropost x 100

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# ダミーユーザ用アカウントと、そのテストデータ
+10.times do |c|
+  _name = "test" + c.to_s
+  user = User.create(name: _name, email: "#{_name}@test.jp", password: "test")
+  100.times do |m|
+    _content = "test post " + m.to_s
+    user.microposts.create(content: _content)
+  end
+end
