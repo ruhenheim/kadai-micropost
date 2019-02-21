3f6a53a2d41503d60914a5ae98997a19ff403210
ruhenheim <asd.uchiyama@gmail.com>
2019-02-21 22:05:22 +0900

    退会処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes, :destroy]
   before_action :set_user, only: [:show, :followings, :followers, :likes]
   def index
     @users = User.all.page(params[:page])
@@ -32,6 +32,17 @@ class UsersController < ApplicationController
   def likes
     @favorites = @user.favorite_microposts.page(params[:page])
     counts @user
+  end
+  def destroy
+    user = User.find(params[:id])
+    if current_user == user
+      session[:user_id] = nil
+      flash[:success] = '退会操作は成功です user.destroy'
+      redirect_to root_url
+    else
+      flash[:danger] = '退会操作は他ユーザへの操作のため出来ません'
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   private
